b716fbe817c22aec92fa5b997512abe9dbbe9d77
Jenny Moon <jenny0moon@gmail.com>
2023-02-17 16:36:57 +0000

    Add screens folders

---
+++ screens/ManageExpense.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+
+function ManageExpense() {
+  return;
+  <Text>Manage Expense</Text>;
+}
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+
+function RecentExpenses() {
+  return;
+  <Text>Recent Expenses</Text>;
+}
+
+export default RecentExpenses;
